f100067ea5e9bd3aa05d44878274de2ed9e5eaf0
unknown <yulian.gaonac@uanl.edu.mx>
2025-08-21 10:38:14 -0600

    laboratorio 3

--- temperatura.R
+++ temperatura.R
@@ -33,10 +33,23 @@ boxplot(datos_meses,
 # Estadisticas descriptivas -----------------------------------------------
 
 
+# crear tres parcelas de 10 datos de cada uno  ----------------------------
+url <- "https://repodatos.atdt.gob.mx/api_update/senasica/actividades_inspeccion_movilizacion/29_
+actividades-inspeccion-movilizacion.csv"
+inspeccion <-read.csv(url)
+head(inspeccion)
+senasica <-read.csv(prof_url_2)
+head(senacisa)
 
+head(conjunto) # muestra las primeras seis filas de laBD
+library(repmis) # No olvidar cargar la paquetería
+conjunto <- source_data("https://www.dropbox.com/s/hmsf07bbayxv6m3/cuadro1.csv?dl=1")
 
-
-
+head(conjunto) # muestra las primeras seis filas de la BD
+library(readr)
+file <- paste0("https://raw.githubusercontent.com/mgtagle/",
+               "202_Analisis_Estadistico_2020/master/cuadro1.csv")
+inventario <- read_csv(file)
 
 
 
